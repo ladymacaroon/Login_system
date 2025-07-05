c3c534405983a9dd46e916b2735c567212886903
Ella Bowker <67073210+ladymacaroon@users.noreply.github.com>
2025-07-05 19:12:48 +0100

    updated

--- fileymcfile.py
+++ fileymcfile.py
@@ -1,4 +1,23 @@
-with open("file.txt", "wt") as file:
-    file.write("Hello\n")
-    file.write("Hello Again")
+import hashlib
 
+credentials = {}
+
+with open("credentials.txt") as cred_file:
+    for line in cred_file:
+       (key, val) = line.split()
+       credentials[key] = val
+
+username = input("username: ")
+password = input("password: ")
+
+password_hash = hashlib.sha256(password.encode('utf-8')).hexdigest()
+
+print(password_hash)
+
+if credentials.get(username) == None:
+    print("username not known")
+else:
+    if credentials[username] == password_hash:
+        print("password valid")
+    else:
+        print("password invalid")
